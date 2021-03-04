a517e4327ef6744d9aa036dce20a6877c2cb2cae
unknown <2444100484@qq.com>
2021-03-04 17:20:05 +0800

    My 2nd commit 2021-3-4

--- Test-CNN-on-CIFAR-10-dataset/main.py
+++ Test-CNN-on-CIFAR-10-dataset/main.py
@@ -1,3 +1,4 @@
+import copy
 import torch
 import torch.nn.functional as fun
 import torch.optim as optim
@@ -15,7 +16,7 @@ BATCH_SIZE_TRAIN = 4
 BATCH_SIZE_TEST = 4
 
 def get_percentage_str(a):
-    return '%d %%' % (100.00 * a)
+    return '%f %%' % (100.00 * a)
 
 def gaussian_weights_init(m):
     classname = m.__class__.__name__
@@ -38,39 +39,51 @@ class CNN(torch.nn.Module):
         #[32,32,3]
         self.conv1 = torch.nn.Conv2d(in_channels=3 , out_channels=128 , kernel_size=7 , stride=1 , padding=1).apply(gaussian_weights_init)
         #[28,28,128]
+        self.norm1 = torch.nn.BatchNorm2d(num_features=128)
+        #[28,28,128]
         self.pool1 = torch.nn.MaxPool2d(kernel_size=2,stride=2)
         #[14,14,128]
         self.conv2 = torch.nn.Conv2d(in_channels=128, out_channels=256, kernel_size=5, stride=1, padding=1).apply(gaussian_weights_init)
-        #[12,12,256]
+        #[12,12,192]
+        self.norm2 = torch.nn.BatchNorm2d(num_features=256)
+        # [12,12,192]
         self.pool2 = torch.nn.MaxPool2d(kernel_size=2, stride=2)
-        # [6*6*256]
+        # [6*6*192]
         self.conv3 = torch.nn.Conv2d(in_channels=256, out_channels=512, kernel_size=3, stride=1, padding=0).apply(gaussian_weights_init)
-        # [4,4,512]
+        # [4,4,256]
+        self.norm3 = torch.nn.BatchNorm2d(num_features=512)
+        # [4,4,256]
         self.pool3 = torch.nn.MaxPool2d(kernel_size=2, stride=2)
-        # [2*2*512]
-        self.fc1 = torch.nn.Linear(512*2*2,1024)
-        self.fc2 = torch.nn.Linear(1024, 256)
+        # [2*2*256]
+        self.dp = torch.nn.Dropout(0.2)
+        self.fc1 = torch.nn.Linear(512*2*2,512)
+        self.fc2 = torch.nn.Linear(512, 256)
         self.fc3 = torch.nn.Linear(256, 64)
         self.fc4 = torch.nn.Linear(64, 10)
 
     def forward(self,x):
-        x=self.conv1(x)
-        x=fun.relu(x)
-        x=self.pool1(x)
+        x = self.conv1(x)
+        x = self.norm1(x)
+        x = fun.relu(x)
+        x = self.pool1(x)
 
         x = self.conv2(x)
+        x = self.norm2(x)
         x = fun.relu(x)
         x = self.pool2(x)
 
         x = self.conv3(x)
+        x = self.norm3(x)
         x = fun.relu(x)
         x = self.pool3(x)
 
         x = x.view(-1 , 2*2*512)
 
+        x = self.dp(x)
         x = self.fc1(x)
         x = fun.relu(x)
 
+        x = self.dp(x)
         x = self.fc2(x)
         x = fun.relu(x)
 
@@ -80,13 +93,14 @@ class CNN(torch.nn.Module):
         x = self.fc4(x)
         return x
 
-def train():
+def train(path):
     accuracyTemp = 0.0
     epoch = 0
     model = CNN().cuda()
     optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
     loss_func = torch.nn.CrossEntropyLoss()
     while (True):
+        model.train()
         times = 0
         running_loss = 0
         for step,data in enumerate(trainLoader):
@@ -104,18 +118,21 @@ def train():
                 print('[%d, %8d] loss: %.3f' %
                       (epoch + 1, times + 1, running_loss / 2000))
                 running_loss = 0.0
+        model.eval()
         accuracyCurrent = val(model)
         if (accuracyTemp > accuracyCurrent):
-            print('This epoch of training reduces the accuracy of the model on the test set . FINISH')
+            print('This epoch of training reduces the accuracy of the model on the test set (' + get_percentage_str(accuracyTemp) + '->' + get_percentage_str(accuracyCurrent) + ')' + ' , the last model is saved . STOP')
             break
         else:
             print('This epoch of training improves the accuracy of the model on the test set ('+get_percentage_str(accuracyTemp)+'->'+get_percentage_str(accuracyCurrent)+')'+' ,  the current model is saved . CONTINUE')
             accuracyTemp = accuracyCurrent
-            torch.save(model.state_dict(), r'.\model.\CNN.pth')
+            torch.save(model, path)
         epoch = epoch+1
-    return model
+    return torch.load(r'.\model\CNN.pth')
+
 
 def val(model):
+    model.eval()
     correct = 0
     total = 0
     with torch.no_grad():
@@ -143,13 +160,10 @@ def testWithId(model,i):
     plt.title(str)
     plt.show()
 
-def loadCNNWithWeight(path):
-    model = CNN().cuda()
-    state_dict = torch.load(path)
-    model.load_state_dict(state_dict)
-    return model
 
-model=train()
-print('Accuracy on test images is: '+ get_percentage_str(val(model)))
+model = train(r'.\model\CNN.pth')
+model.eval()
+print(val(model))
+
 
 
